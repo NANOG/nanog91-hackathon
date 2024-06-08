d848cb4ca9f9d9c53f85b19d91e861b174d110ff
Steve Feldman <feldman@twincreeks.net>
2024-06-07 20:13:09 -0700

    merge candidate

--- scenario_nautobot/scenario_nautobot/score.py
+++ scenario_nautobot/scenario_nautobot/score.py
@@ -7,6 +7,7 @@ import sys
 import pynautobot
 # import colorama
 from colorama import Fore, Back, Style
+from pprint import pprint
 
 def score_hash(*values):
     string = '|'.join(str(v) for v in values)
@@ -80,7 +81,109 @@ def score_1_4(nb, verbose=False):
 
 
 def score_1_5(nb, verbose=False):
-    return(score_hash("leaf4"))
+    query1 = """
+{
+  prefixes(
+    namespace: "hackathon-template-scenario"
+    prefix: ["10.10.0.4/31", "10.10.1.4/31", "2001:db8:10:10::4/127", "2001:db8:10:10:1::4/127"]
+  ) {
+    prefix
+    type
+  }
+}
+"""
+
+    query2 = """
+{
+  devices(name: "leaf3") {
+    name
+    interfaces(name: ["Ethernet2", "Ethernet3", "Loopback0", "Management0"]) {
+      name
+      connected_interface {
+        device {
+          name
+        }
+        name
+      }
+      ip_addresses {
+        ip_version
+        address
+        parent {
+          namespace {
+            name
+          }
+          prefix
+        }
+      }
+    }
+  }
+}
+"""
+    response1 = nb.graphql.query(query=query1)
+    if verbose:
+        print(f"return code={response1.status_code}")
+        pprint(response1.json)
+    prefixes = response1.json['data']['prefixes']
+
+    response2 = nb.graphql.query(query=query2)
+    if verbose:
+        print(f"return code={response2.status_code}")
+        pprint(response2.json)
+    interfaces = response2.json['data']['devices'][0]['interfaces']
+    return(score_hash(prefixes, interfaces))
+
+
+def score_2_1(nb, verbose=False):
+    print("Challenge 2.1 requires a value printed by your script.")
+    print("See the challenge description for details.")
+    return None
+
+def score_2_2(nb, verbose=False):
+    query2 = """
+{
+  devices(name: "test-switch") {
+    name
+    role {
+      name
+    }
+    device_type {
+      manufacturer {
+        name
+      }
+      model
+      part_number
+      u_height
+    }
+  }
+}
+"""
+    response = nb.graphql.query(query=query2)
+    if verbose:
+        print(f"return code={response.status_code}")
+        print(response.json)
+    devices = response.json['data']['devices']
+
+    return score_hash(devices[0])
+    # devices = response.json['data']['devices']
+    # device = {}
+    # for dev in devices:
+    #     device[dev['name']] = dev
+    # if verbose:
+    #     print(f"device={device}")
+
+    # if 'leaf3' not in device or 'leaf4' not in device:
+    #     print("Unable to find one or both new devices.")
+    #     return None
+    
+    # return score_hash(device['leaf3'], device['leaf4'])
+
+def score_2_4(nb, verbose=False):
+    dev = nb.dcim.device_types.get(model="vEOS")
+    if dev is None:
+        return score_hash("Success for 2.4!")
+    else:
+        print("vEOS device type still exists.")
+        return None
 
 
 RO_TOKEN = "1dc0438033a3b624e2ddc92995d7d4cd1bdee69a"
@@ -92,6 +195,10 @@ challenges = {
     "1.3": { "token": RW_TOKEN, "scorer": score_1_3 },
     "1.4": { "token": RW_TOKEN, "scorer": score_1_4 },
     "1.5": { "token": RW_TOKEN, "scorer": score_1_5 },
+    "2.1": { "token": None,     "scorer": score_2_1 },
+    "2.2": { "token": RW_TOKEN, "scorer": score_2_2 },
+    "2.3": { "token": RW_TOKEN, "scorer": score_2_2 },  # Same as 2.2!
+    "2.4": { "token": RW_TOKEN, "scorer": score_2_4 },
 }
 
 def main():
@@ -104,7 +211,7 @@ def main():
         print(f"Challenge {args.challenge} not implemented")
         return 1
 
-    nbserver = os.environ.get("NBSERVER", "http://localhost:80")
+    nbserver = os.environ.get("NB_URL", "http://localhost:80")
     c = challenges[args.challenge]
     if "token" in c and c["token"]:
         try:
